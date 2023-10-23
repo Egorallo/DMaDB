e50fa22dc6d849db41361283e67d0cd19fa98274
Egorallo <sea25042003@gmail.com>
2023-10-24 02:50:34 +0300

    added basic queries

---
+++ queries/queries.sql
@@ -0,0 +1,45 @@
+SELECT    *
+FROM      transaction;
+
+SELECT    name
+FROM      users
+WHERE     LOWER(name) LIKE 'm%';
+
+SELECT    surname
+FROM      users
+WHERE     UPPER(surname) LIKE '%Z%';
+
+SELECT    id,
+          maintenance_due
+FROM      fuel_dispenser
+ORDER BY  maintenance_due DESC;
+
+SELECT    *
+FROM      card
+LIMIT     5;
+
+SELECT    *
+FROM      users
+LIMIT     10
+OFFSET    50;
+
+SELECT    SUM(total_cost)
+FROM      transaction;
+
+SELECT    MIN(rating)
+FROM      review;
+
+SELECT    *
+FROM      fuel_inventory
+WHERE     quantity > 5000;
+
+SELECT    DISTINCT fuel_inventory_id
+FROM      fuel_dispenser;
+
+SELECT    *
+FROM     
+WHERE     fuel_sold BETWEEN 4000 AND 5000;
+
+SELECT    *
+FROM      fuel_dispenser
+WHERE     fuel_inventory_id IN (3, 4, 5);
