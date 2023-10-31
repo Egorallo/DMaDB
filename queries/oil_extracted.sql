f320624c4e6d04f6d6b2941223a4c532db207cd8
Egorallo <sea25042003@gmail.com>
2023-11-01 01:36:31 +0300

    added joins, groupby, partitions

---
+++ queries/oil_extracted.sql
@@ -0,0 +1,54 @@
+insert into
+    oil_extracted (amount, region)
+values
+    (1, 'Africa');
+
+insert into
+    oil_extracted (amount, region)
+values
+    (2, 'Europe');
+
+insert into
+    oil_extracted (amount, region)
+values
+    (3, 'Asia');
+
+insert into
+    oil_extracted (amount, region)
+values
+    (4, 'Africa');
+
+insert into
+    oil_extracted (amount, region)
+values
+    (5, 'Africa');
+
+insert into
+    oil_extracted (amount, region)
+values
+    (6, 'Africa');
+
+insert into
+    oil_extracted (amount, region)
+values
+    (7, 'Europe');
+
+insert into
+    oil_extracted (amount, region)
+values
+    (8, 'Asia');
+
+insert into
+    oil_extracted (amount, region)
+values
+    (9, 'Asia');
+
+insert into
+    oil_extracted (amount, region)
+values
+    (10, 'Asia');
+
+insert into
+    oil_extracted (amount, region)
+values
+    (11, 'Asia');
--- queries/queries.sql
+++ queries/queries.sql
@@ -82,4 +82,116 @@ SELECT
 FROM
   fuel_inventory inv
   INNER JOIN fuel_type ftype ON inv.fuel_type_id = ftype.id;
+
+--------------
+SELECT
+  users.surname,
+  SUM(transaction.total_cost) as total
+FROM
+  users
+  JOIN transaction ON users.id = transaction.user_id
+WHERE
+  transaction.date >= current_date - interval '30 days'
+GROUP BY
+  users.surname
+HAVING
+  SUM(transaction.total_cost) > 100;
+
+SELECT
+  users.surname,
+  transaction.total_cost AS most_spent
+FROM
+  users
+  INNER JOIN transaction ON transaction.user_id = users.id
+WHERE
+  total_cost = (
+    SELECT
+      MAX(total_cost)
+    FROM
+      transaction
+  );
+
+SELECT
+  fuel_dispenser.status,
+  fuel_inventory.price_per_unit
+FROM
+  fuel_dispenser
+  INNER JOIN fuel_inventory ON fuel_dispenser.fuel_inventory_id = fuel_inventory.id;
+
+SELECT
+  fuel_dispenser.status,
+  fuel_inventory.price_per_unit
+FROM
+  fuel_dispenser
+  LEFT OUTER JOIN fuel_inventory ON fuel_dispenser.fuel_inventory_id = fuel_inventory.id;
+
+SELECT
+  fuel_dispenser.status,
+  fuel_inventory.price_per_unit
+FROM
+  fuel_dispenser
+  CROSS JOIN fuel_inventory;
+
+SELECT
+  fuel_dispenser.status,
+  fuel_inventory.price_per_unit
+FROM
+  fuel_dispenser FULL FULL
+  JOIN fuel_inventory ON fuel_dispenser.fuel_inventory_id = fuel_inventory.id;
+
+SELECT
+  fuel_dispenser_id,
+  AVG(total_cost) AS avg_bill
+FROM
+  transaction
+GROUP BY
+  fuel_dispenser_id
+HAVING
+  AVG(total_cost) > (
+    SELECT
+      AVG(total_cost)
+    FROM
+      transaction
+  );
+
+SELECT
+  user_id,
+  id,
+  total_cost,
+  SUM(total_cost) OVER (PARTITION BY user_ id) AS user_total
+FROM
+  transaction;
+
+SELECT
+  id,
+  fuel_dispenser_id,
+  fuel_dispensed,
+  ROW_NUMBER() OVER (
+    PARTITION BY fuel_dispenser_id
+    ORDER BY
+      fuel_dispensed DESC
+  )
+FROM
+  transaction;
+
+
+CREATE TABLE oil_extracted (
+  id SERIAL,
+  amount INT,
+  region VARCHAR(50),
+  PRIMARY KEY (id, region)
+) PARTITION BY LIST (region);
+
+CREATE TABLE Africa PARTITION OF oil_extracted FOR
+VALUES
+  IN ('Africa');
+
+CREATE TABLE Europe PARTITION OF oil_extracted FOR
+VALUES
+  IN ('Europe');
+
+CREATE TABLE Asia PARTITION OF oil_extracted FOR
+VALUES
+  IN ('Asia');
+
   
